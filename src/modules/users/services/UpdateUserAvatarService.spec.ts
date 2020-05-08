d323565ad42c51928a13fadcc2f9ebe384560ac7
Leonardo Sampaio Cairo <leocairos@gmail.com>
2020-05-08 19:41:40 -0300

    Updated User Module - Partial

--- src/modules/users/services/UpdateUserAvatarService.spec.ts
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -37,7 +37,7 @@ describe('UpdateAvatar', () => {
       fakeStorageProvider,
     );
 
-    expect(
+    await expect(
       updateUserAvatar.execute({
         user_id: 'non-existing-user',
         avatarFilename: 'avatar.jpg',
