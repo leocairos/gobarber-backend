a68858059fbd2e586ae41aba83877de24a1e8b2f
Leonardo Sampaio Cairo <leocairos@gmail.com>
2020-05-14 12:19:11 -0300

    Finalizando back-end do app - Prestador de serviço

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,18 +2,15 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appointementsRouter = Router();
 const appointementsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointementsRouter.use(ensureAuthenticated);
 
-// appointementsRouter.get('/', async (request, response) => {
-//   const appointements = await appointmentsRepository.find();
-
-//   return response.json(appointements);
-// });
-
 appointementsRouter.post('/', appointementsController.create);
+appointementsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointementsRouter;
